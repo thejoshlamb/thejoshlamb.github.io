dc55b1ba2dbb0a75763a3234835fb4228c616ece
josh.lamb <josh.lamb@hootsuite.com>
2018-03-08 12:42:39 -0800

    add more info to burger list, add obnoxious blink

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var multiplier = 12345;
+const multiplier = 12345;
 
-var burgers = {
+const burgers = {
     'Trans Am': 8.85,
     'Moderne Burger': 6.675,
     'Bistro Wagon rouge': 8.2333,
@@ -12,17 +12,74 @@ var burgers = {
     'Pourhouse': 6.45
 };
 
+const list = [
+    {
+        name: 'Trans Am',
+        score: 8.85,
+        date: new Date('January 24 2018'),
+        link: 'https://www.instagram.com/transamrestaurant/'
+    },
+    {
+        name: 'Moderne Burger',
+        score: 6.675,
+        date: new Date('January 20 2017'),
+        link: 'https://www.moderneburger.com/'
+    },
+    {
+        name: 'Bistro Wagon Rouge',
+        score: 8.2333,
+        date: new Date('November 29 2017'),
+        link: 'http://bistrowagonrouge.com/'
+    },
+    {
+        name: 'Monarch Burger at The American',
+        score: 8.8,
+        date: new Date('June 14 2017'),
+        link: 'https://www.monarchburger.com/'
+    },
+    {
+        name: 'Campagnolo Upstairs',
+        score: 8.4,
+        date: new Date('March 15 2017'),
+        link: 'https://www.campagnolorestaurant.ca/upstairs-campagnolo/'
+    },
+    {
+        name: 'The Oakwood Burger 2.0',
+        score: 8.1,
+        date: new Date('July 26 2017'),
+        link: 'http://www.theoakwood.ca/'
+    },
+    {
+        name: 'Parallel 49 Street Kitchen',
+        score: 6.85,
+        date: new Date('September 27 2017'),
+        link: 'http://parallel49brewing.com/street_kitchen'
+    },
+    {
+        name: 'Hawksworth',
+        score: 8.25,
+        date: new Date('September 20 2016'),
+        link: 'https://hawksworthrestaurant.com/'
+    },
+    {
+        name: 'Pourhouse',
+        score: 6.45,
+        date: new Date('October 27 2017'),
+        link: 'http://www.pourhousevancouver.com/'
+    },
+];
+
 const numberWithCommas = function(x) {
     return Math.round(x).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+};
 
-var table = document.getElementById('scoresTable');
-var hiscores = [];
-for (var item in burgers) {
-    hiscores.push([item, burgers[item]*multiplier]);
-}
+const table = document.getElementById('scoresTable');
+hiscores = list.map(function(item) {
+    item.score = item.score*multiplier;
+    return item;
+});
 hiscores.sort(function(a, b) {
-    return b[1] - a[1];
+    return b.score - a.score;
 });
 var count = 1;
 hiscores.forEach(function(burger) {
@@ -31,7 +88,12 @@ hiscores.forEach(function(burger) {
     var name = row.insertCell(1);
     var score = row.insertCell(2);
     rank.innerText = count;
-    name.innerText = burger[0];
-    score.innerText = numberWithCommas(burger[1]);
+    var link = document.createElement("a");
+    link.setAttribute("href", burger.link);
+    link.className = "blink";
+    var linkText = document.createTextNode(burger.name);
+    link.appendChild(linkText);
+    name.appendChild(link);
+    score.innerText = numberWithCommas(burger.score);
     count += 1;
 });
